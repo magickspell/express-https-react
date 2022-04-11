d4b54913a9d1b22eb1023f80a110edc3d6008e47
magickspell <warface1996@yandex.ru>
2022-04-11 15:44:31 +0300

    vitamin api

--- index.js
+++ index.js
@@ -39,16 +39,21 @@ app.get('/', function (req, res) {
 app.get('/api', (req, res) => {
   res.sendFile(path.join(__dirname, 'api', 'test.json'))
 })
+app.get('/vitamins_api.php', (req,res) => {
+  res.status(200).sendFile(path.join(__dirname, 'api', 'prodVitamins.json'))
+})
+
 
 //auth // u need body-parser
 const login = 'admin';
 const password = 'admin';
-app.post('/login', function (req, res) {
+app.post('/login', (req, res) => {
   console.log(req.body)
   const auth = { login: req.body.login, password: req.body.password }
   if (auth.login === login && auth.password === password) {
     //res.set('success')
     res.status(200).send({
+      username: req.body.login,
       refreshToken: 'G5-RVeu0TjIH-hsXp1TvEhn2figiocnedtFu7GV1Hbo',
       accessToken: 'rxk-X3rnAy9Yginy-Ieb_z3szPHAmzowqEj6bdo1eG0',
       isActivated: 'true',
@@ -64,6 +69,46 @@ app.get('/login', (req, res) => {
   res.status(404).send('login get')
 })
 
+// questions
+const totalQuest = 2;
+const userAnswers = {
+  username: 'admin',
+  answers: []
+}
+app.post('/answers', (req, res) => {
+  if(req.body.username) {
+    res.status(200).send({
+      answers: [{questionId: 1, iscomplete: true, answer: false, advice: 'откажитесь от сладкого'}]
+    })
+  }
+  else {
+    res.status(500).send('something went wrong on server')
+  }
+})
+app.post('/question', (req, res) => {
+  res.status(200).send({
+    question: '1. Болит ли у Вас голова?',
+    questionId: 1,
+    totalQuest: totalQuest
+  })
+})
+app.post('/questions', (req, res) => {
+  if (req.body.questionId < 2) {
+    res.status(200).send({
+      question: '2. Высыпаетесь ли Вы?',
+      questionId: 2,
+      totalQuest: totalQuest
+    })
+  }
+  else if (req.body.questionId >= totalQuest) {
+    res.status(200).send(
+      'Вы прошли тест'
+    )
+  }
+  else {
+    res.status(500).send('something went wrong on server')
+  }
+})
 
 const server = https.createServer(options, app)
 server.listen(PORT, () => { console.log('server started') })
@@ -71,3 +116,4 @@ server.listen(PORT, () => { console.log('server started') })
 // npm run-script nodemon - start app
 // https://localhost:3443/
 // sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./selfsigned.key -out selfsigned.crt
+// check antivirus allow rules
